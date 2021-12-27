67587f20984fa5d35a80d565f43e79f5dd27cc49
Craig <indigomx9@gmail.com>
2021-12-26 22:25:00 -0500

    Adding the React front-end.

---
+++ client/src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus facere cum voluptatem. Ratione enim impedit animi quod ipsa illum soluta inventore facere aliquam repellat, nisi minima sapiente ab perferendis nostrum?</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
